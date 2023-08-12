273f8c73e947b44e60a6fbee1829df0db63a579a
tyama01 <tyama@inl.ics.keio.ac.jp>
2023-08-12 20:20:44 +0900

    commit

--- apps/graph.py
+++ apps/graph.py
@@ -32,9 +32,49 @@ class DataLoader:
 
     def get_communities(self):
         return self.c_id, self.id_c
+    
+class CommunityGraph:
+    def __init__(self, G, c_id, id_c):
+        self.G = G
+        self.c_id = c_id
+        self.id_c = id_c
+        self.c_edge = {}
+        self.c_G = nx.Graph()
+
+    def generate_community_graph(self):
+        for c_num in range(len(self.c_id)):
+            c_num_size = len(self.c_id[c_num])
+            for i in range(c_num_size):
+                
+                # 全頂点を1ホップ進める
+                neighbors = list(self.G.neighbors(self.c_id[c_num][i]))
+                
+                # 頂点 i の隣接頂点を１つずつ調べる             
+                for neigh in neighbors: 
+                    
+                    # 隣接頂点が同じコミュニティなら for ループを抜ける
+                    if self.id_c[neigh] == c_num:
+                        continue
+
+                    # コミュニティ間のエッジ関係を把握    
+                    key = (c_num, self.id_c[neigh])
+                    if key in self.c_edge:
+                        self.c_edge[key] += 1
+                    else:
+                        self.c_edge[key] = 1
+
+        c_edge_key = list(self.c_edge.keys())
+
+        for e in c_edge_key:
+            self.c_G.add_edge(e[0], e[1])
+
+        return self.c_G
+
 
 class RandomWalk:
-    def random_walk(self, G, v, walk_num, v_walk_num_cnt):
+    
+    # ひたすら隣接頂点にランダムに遷移
+    def simple_random_walk(self, G, v, walk_num, v_walk_num_cnt):
         for i in range(walk_num + 1):
             if v in v_walk_num_cnt:
                 v_walk_num_cnt[v] += 1
@@ -46,7 +86,9 @@ class RandomWalk:
             v = neighbors[random_index]
 
         return v_walk_num_cnt
-
+    
+    
+    # 確率 d で　隣接頂点にランダムに遷移、　確率 1 - d でランダムな頂点に遷移
     def pr_random_walk(self, G, v, walk_num, v_walk_num_cnt, d):
         node_list = list(G.nodes)
 
@@ -68,6 +110,8 @@ class RandomWalk:
 
         return v_walk_num_cnt
     
+    
+    # 媒介中心性ベースの RW
     def random_walk_betweenness_centrality(self, G, num_walks=10000, walk_length=1000):
         betweenness_centrality = {node: 0.0 for node in G.nodes()}
 
