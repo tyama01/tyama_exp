8f6258b0e2b604a0f80d021bc7c0b7d3bac4be59
tyama01 <tyama@inl.ics.keio.ac.jp>
2023-08-13 14:41:11 +0900

    commit

--- apps/rwer_flow.py
+++ apps/rwer_flow.py
@@ -26,6 +26,26 @@ print("-----------------------------------")
 # --------------- コミュニティグラフ生成 ------------------
 community_graph_obj = CommunityGraph(G, c_id, id_c)
 c_G = community_graph_obj.generate_community_graph()
+community_size = community_graph_obj.get_community_size()
 print(c_G)
+#print(community_size)
 print("-----------------------------------")
+
+# プロット
+node_size = []
+node_color = []
+
+# カラーコード選択
+color_util_obj = ColorUtil()
+num_colors = 100
+label2color = color_util_obj.choose_colors(num_colors)
+
+
+for i in range(len(community_size)):
+    node_size.append(community_size[i])
+    node_color.append(label2color[i+ int((i+5)/2)])
+
+pos = nx.circular_layout(c_G)
+nx.draw(c_G, pos, node_color=node_color, node_size=node_size, with_labels=True)
+plt.show()
 # ---------------------------------------------------
